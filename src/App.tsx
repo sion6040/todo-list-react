3375d9072437d31db1f19643c09c0025322b6b3e
sion6040 <mahumahu60400@docomo.ne.jp>
2021-03-16 11:27:36 +0900

    create new task button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import {useState} from "react";
-
-
+//divタグをインラインにしているため、widthの指定ができずに、完了ﾎﾞﾀﾝがずれてしまう。
+let idnum:number=3; //length+1だとIdがかぶってしまって、識別方法が分からなかったため使用
 export function App() {
   const [todolist,setTodolist]=useState([
   {id:1,task:"書類を作る",type:"WORK"},
@@ -12,15 +12,25 @@ export function App() {
   const handleRemoveItem = (e:number)=> {
     setTodolist(todolist.filter(todo => todo.id !== e));
   };
+  const createTask=()=>{
+    idnum++;
+    setTodolist(todolist.concat({
+      id:idnum,task:"NEW TASK",type:"WORK"
+    }));
+  }
   return (
     <div >
       <div className="body">
     {todolist.map((todo)=>(
       <div className="tasklist">
-        <p>{todo.type}　{todo.task}</p>
-        <button onClick={()=>handleRemoveItem(todo.id)} key={todo.id}>完了</button>
+        <div style={{margin:"10px",display:"inline"}}>{todo.type}</div>
+        <div style={{margin:"10px",display:"inline"}}>{todo.task}</div>
+        <div style={{padding:"0 0 0 250px",display:"inline"}}><button style={{width:"90px"}} onClick={()=>handleRemoveItem(todo.id)} key={todo.id}>完了</button></div>
       </div>
     ))}
+    <div className="createTask">
+      <button style={{width:"100px",height:"50px",marginBottom:"400px"}} onClick={()=>createTask()}>タスクを追加</button>
+    </div>
       </div>
     </div>
   );
